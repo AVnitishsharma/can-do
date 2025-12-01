bbdc4b85c67c740fb8e517efbac6ec35ec60df34
Nitish Sharma <ernitishsharma361@gmail.com>
2025-12-01 21:11:08 +0530

    count active

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let tasksData = {}
+
 const todo = document.querySelector("#todo")
-const progres = document.querySelector("#progress")
+const progress = document.querySelector("#progress")
 const done = document.querySelector("#done")
 const tasks = document.querySelectorAll(".task")
 const toggleModalButton = document.querySelector("#toggle-modal")
@@ -10,8 +12,43 @@ const taskTitle = document.querySelector("#task-title")
 const taskDescription = document.querySelector("#task-description")
 const deletebox = document.querySelector(".delete")
 
+if (localStorage.getItem("tasks")) {
+  const data = JSON.parse(localStorage.getItem("tasks"));
+  console.log(data)
 
+  for(const col in data){
+    const column = document.querySelector(`#${col}`);
+    data[col].forEach(task =>{
+        const div = document.createElement("div")
+        div.classList.add("task")
+        div.setAttribute("draggable","true")
+        div.innerHTML =`
+                <h2>${taskTitle.value}</h2>
+                <p>${taskDescription.value}</p> `
+          
+        todo.appendChild(div)
+    })
+  }
+}
 let dragElement = null
+
+// Function to update task counts in all columns
+function updateTaskCounts() {
+  [todo, progress, done].forEach(col => {
+    const tasksInCol = col.querySelectorAll(".task")
+    const count = col.querySelector(".right")
+    count.innerText = tasksInCol.length;
+
+    tasksData[col.id] = Array.from(tasks).map(t =>{
+      return{
+        title: t.querySelector("h2").innerText,
+        description: t.querySelector("p").innerText
+      }
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasksData));
+  })
+}
+
 
 tasks.forEach(task => {
   task.addEventListener("drag", (e) => {
@@ -19,6 +56,7 @@ tasks.forEach(task => {
   })
 });
 
+// task dragable logic
 function addDragEventOnColum(column) {
   column.addEventListener("dragenter", (e) =>{
     e.preventDefault();
@@ -34,13 +72,17 @@ function addDragEventOnColum(column) {
   })
   column.addEventListener("drop", (e) =>{
     e.preventDefault();
+    
+    updateTaskCounts();
     column.appendChild(dragElement);
     column.classList.remove("hoverOver")
+
+    
   })
 
 }
 addDragEventOnColum(todo);
-addDragEventOnColum(progres);
+addDragEventOnColum(progress);
 addDragEventOnColum(done);
 
 toggleModalButton.addEventListener("click", () =>{
@@ -50,6 +92,7 @@ closeButton.addEventListener("click", () => {
   modal.style.display = "none"
 })
 
+// new task creater logic
 addTaskBtn.addEventListener(("click"), () => {
 
   const div = document.createElement("div")
@@ -65,4 +108,8 @@ addTaskBtn.addEventListener(("click"), () => {
   div.addEventListener("drag", (e) => {
     dragElement = div
   })
+
+  updateTaskCounts();
 })
+
+updateTaskCounts(); // Initial count update on page load
